e31d94200a4c66666250a30e304c72f4a512968c
abhishek <abhisheksingh16031@gmail.com>
2026-09-14 12:44:40 +0530

    adding the for loop function in program.cs file.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Hello, World!";
+        
+        for (int i = 0; i < 30; i++)
+        {
+            Console.Clear(); // clear screen each frame
+            Console.SetCursorPosition(i, 10); // move position
+            Console.WriteLine(text);
+            Thread.Sleep(100); // control speed
+        }
+    }
+}
