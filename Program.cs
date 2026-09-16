7f29942643cf9c637a6ad147f2eb857ac10a1aa1
abhishek <abhisheksingh16031@gmail.com>
2026-09-16 06:41:40 +0530

    changing the program.cs and Dockerfule file

--- Program.cs
+++ Program.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Threading;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
 
-class Program
-{
-    static void Main()
-    {
-        string text = "Hello, World!";
-        
-        for (int i = 0; i < 30; i++)
-        {
-            Console.Clear(); // clear screen each frame
-            Console.SetCursorPosition(i, 10); // move position
-            Console.WriteLine(text);
-            Thread.Sleep(100); // control speed
-        }
-    }
-}
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// main endpoint
+app.MapGet("/", () => "Hello from ECS on port 8080 🚀");
+
+// health check (important for ECS/ALB)
+app.MapGet("/health", () => Results.Ok("Healthy"));
+
+// force port 8080
+app.Run("http://0.0.0.0:8080");
--- dockerfile
+++ dockerfile
@@ -9,4 +9,7 @@ RUN dotnet publish -c Release -o /app
 FROM mcr.microsoft.com/dotnet/aspnet:8.0
 WORKDIR /app
 COPY --from=build /app .
+
+ENV ASPNETCORE_URLS=http://+:8080
+
 ENTRYPOINT ["dotnet", "HelloWorldApp.dll"]
